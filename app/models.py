bbfdbc7d76009b56a9b1ad3eea31c1b593a0e5f5
jude-LafiteIII <judewrs@gmail.com>
2018-06-30 15:49:27 +0100

    task stuff ig

--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ class SimpleUser(db.Model):
     username = db.Column(db.String)
     email = db.Column(db.String, index=True, unique=True)
     user_id = db.Column(db.Integer, unique=True)
+    subscriptions = db.Column(db.String)
