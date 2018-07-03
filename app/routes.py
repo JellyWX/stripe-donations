8a1876ef777f0440826a2d15ff067118f232e084
jude-LafiteIII <judewrs@gmail.com>
2018-07-03 23:33:53 +0100

    subscription pulling stuff

--- app/routes.py
+++ app/routes.py
@@ -15,9 +15,9 @@ def index():
 
     if SimpleUser.query.filter_by(email=discord_user['email']).first() is None: # check if the user is registered already
 
-        user = SimpleUser(user_id=discord_user['id'], email=discord_user['email'], username=discord_user['username'])
+        user = SimpleUser(user_id=discord_user['id'], email=discord_user['email'], username=discord_user['username']) # if not make a simple log of their details
 
-        db.session.add(user)
+        db.session.add(user) # add to DB
         db.session.commit() # commit to the DB
 
     return render_template('index.html', key=app.config['STRIPE_PUBLIC'])
@@ -38,14 +38,15 @@ def charge():
     charge = stripe.Subscription.create(
         customer=customer.id,
         items=[
-            {'plan': 'plan_D3QO1EpbGsFVMK'}
+            {'plan': 'plan_D5INAPYHTis1bc'}
         ]
     )
 
     return render_template('charge.html')
 
+
 @app.route('/api/user/email/<int:id>', methods=['GET'])
-def user_subscriptions(id):
+def user_email(id):
     user = SimpleUser.query.filter_by(user_id=id).first()
 
     if user is None:
@@ -53,3 +54,22 @@ def user_subscriptions(id):
 
     else:
         return jsonify({'id': user.user_id, 'email': user.email})
+
+
+@app.route('/api/user/subscriptions/<int:id>', methods=['GET'])
+def user_subscriptions(id):
+    user = SimpleUser.query.filter_by(user_id=id).first()
+
+    if user is None:
+        abort(404)
+
+    else:
+        customer = dict(stripe.Customer.list(email=user.email, limit=100))['data']
+
+        subscriptions = []
+
+        for cus in customer:
+            for sub in cus['subscriptions']['data']:
+                subscriptions.append(sub['plan']['nickname'])
+
+        return jsonify(subscriptions)
